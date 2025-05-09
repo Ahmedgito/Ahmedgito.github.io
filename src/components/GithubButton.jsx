bc88a3063bbaaf6255468f3983aec310d19f423f
Ahmedgito <mamooabid1@gmail.com>
2025-05-09 14:55:26 +0500

    style: Adjust positioning of the GitHub button for better alignment

--- src/components/GithubButton.jsx
+++ src/components/GithubButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
   return (
-    <button href="https://github.com/Ahmedgito" className="flex overflow-hidden items-center text-sm font-medium border-1 border-amber-50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-46 md:left-12 left-0 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2">
+    <button href="https://github.com/Ahmedgito" className="flex overflow-hidden items-center text-sm font-medium border-1 border-amber-50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-46 md:left-0 left-0 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2">
       <span className="absolute  -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
       <div className="flex items-center">
         <svg className="w-4 h-4 fill-current" viewBox="0 0 438.549 438.549">
--- src/section/navbar/Navbar.jsx
+++ src/section/navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.4 }}
-          className="md:block hidden"
+          className="md:block hidden "
         >
           <a
             target="_blank"
